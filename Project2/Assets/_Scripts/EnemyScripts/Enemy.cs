5bd50029f8d9e950205881314542be6c7a2d73d3
adamsahelii <adamsaheliali@gmail.com>
2025-11-26 14:43:30 +0200

    after fixing enemy not zombie

---
+++ Project2/Assets/_Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace _Scripts.EnemyScripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private int HP = 100;
+        private Animator animator;
+    
+        private NavMeshAgent navAgent;
+    
+    
+    
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            navAgent = GetComponent<NavMeshAgent>();
+        }
+
+        public void TakeDamage(int damageAmount)
+        {
+            HP -= damageAmount;
+
+            if (HP <= 0)
+            {
+                int radnomValue = Random.Range(0, 2);// 0 or 1
+                if (radnomValue == 0)
+                {
+                    animator.SetTrigger("DIE1");
+                }
+                else
+                {
+                    animator.SetTrigger("DIE2");
+                }
+            }
+            else
+            {
+                animator.SetTrigger("DAMAGE");
+            }
+        }
+
+        private void onDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, 2.5f);
+        
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, 18f);
+        
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, 21f);
+        }
+    }
+}
--- Project2/Assets/_Scripts/Weapons/Bullet.cs
+++ Project2/Assets/_Scripts/Weapons/Bullet.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
 
+using _Scripts.EnemyScripts; 
+
 namespace _Scripts.Weapons
 {
     public class Bullet : MonoBehaviour
     {
-        [SerializeField] float timeToDestory;
+        [SerializeField] float timeToDestory = 2f;
         float _timer;
-        
+
+        int _damage;
+        public void SetDamage(int dmg) => _damage = dmg;
+
         void Update()
         {
             _timer += Time.deltaTime;
-            if (_timer > timeToDestory) Destroy(gameObject);
+            if (_timer > timeToDestory)
+                Destroy(gameObject);
         }
-        
+
         private void OnCollisionEnter(Collision collision)
         {
-            Destroy(this.gameObject);
+            if (collision.gameObject.CompareTag("Zombie"))
+            {
+                Enemy zombie = collision.gameObject.GetComponent<Enemy>();
+                if (zombie != null)
+                {
+                    zombie.TakeDamage(_damage);
+                }
+            }
+
+            Destroy(gameObject);
         }
     }
 }
