aa43bcf1ac6f6dd4d6c4758aae5f52886806fd3b
adamsahelii <adamsaheliali@gmail.com>
2025-11-26 19:59:54 +0200

    added zombie waves , zombie spawners etc...

--- Project2/Assets/_Scripts/Player.cs
+++ Project2/Assets/_Scripts/Player.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
    public int HP = 100;
+   public GameObject bloodyScreen;
+   public Image fadeImage;
+   
    
    public void TakeDamage(int damageAmount)
    {
@@ -17,14 +23,50 @@ public class Player : MonoBehaviour
        else
        {
            print("Player hit");
+           StartCoroutine(BloodyScreenEffect());
        }
    }
 
-   private void onTriggerEnter(Collider other)
+   private IEnumerator BloodyScreenEffect()
+   {
+       if (bloodyScreen.activeInHierarchy == false)
+       {
+           bloodyScreen.SetActive(true);
+       }
+       
+       var image = bloodyScreen.GetComponentInChildren<Image>();
+
+       Color startColor = image.color;
+       startColor.a = 1f;
+       image.color = startColor;
+
+       float duration = 3f;
+       float elapsedTime = 0f;
+
+       while (elapsedTime < duration)
+       {
+           float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+
+           Color newColor = image.color;
+           newColor.a = alpha;
+           image.color = newColor;
+           elapsedTime += Time.deltaTime;
+           
+           yield return null;
+       }
+       
+       
+       if (bloodyScreen.activeInHierarchy)
+       {
+           bloodyScreen.SetActive(false);
+       }
+   }
+   
+   private void OnTriggerEnter(Collider other)
    {
        if (other.CompareTag("ZombieHand"))
        {
-           TakeDamage(25);
+           TakeDamage(other.gameObject.GetComponent<ZombieHand>().damage);
        }
    }
    
---
+++ Project2/Assets/_Scripts/ZombieHand.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class ZombieHand : MonoBehaviour
+    {
+        public int damage;
+    
+    
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
